b3c4000fbbd6d582cb5cc8b00e26efb1ca032d19
navneetgpro <navneetguptapro@gmail.com>
2020-06-26 05:55:59 +0530

    baseurl in appjs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
-const baseurl = window.location.origin;
-// const baseurl = 'http://localhost/onlinergc';
+// const baseurl = window.location.origin;
+const baseurl = 'http://localhost/onlinergc';
 // Make Sure SW are supporated
 /*  if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
